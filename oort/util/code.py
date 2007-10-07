c2cd418638ee59a317941711507b16662a8a9e95
lindstream <lindstream@localhost>
2007-10-07 14:14:22 +0000

    Updated documentation; minor code cleanup.

--- oort/util/code.py
+++ oort/util/code.py
@@ -2,8 +2,14 @@
 
 
 class autosuper(type):
-    def __init__(cls, name, bases, dict):
-        super(autosuper, cls).__init__(name, bases, dict)
+    """
+    Use this as a metaclass for types to set the magic attribute '__super' on
+    them. When this is accessed through self the result is an object
+    constructed as if super(ThisType, self) had been called. (Makes it a little
+    bit more like in Python 3, where super() will work like this attribute.)
+    """
+    def __init__(cls, name, bases, members):
+        super(autosuper, cls).__init__(name, bases, members)
         setattr(cls, "_%s__super" % name, super(cls))
 
 
